02e111c218646fa124641acc5251b70591c1d51d
Hamza El Massoudi <hmz.elm@outlook.sa>
2023-11-26 13:38:11 +0100

    New : Client entity

--- src/main/java/com/dbconnection/dao/Entities/Client.java
+++ src/main/java/com/dbconnection/dao/Entities/Client.java
@@ -6,16 +6,19 @@ public class Client {
     private String prenom;
     private String email;
     private String address;
+    private String password;
 
-    public Client() {
+    public Client(int i, String string, String string2, String string3, String string4, String string5,
+            String string6) {
     }
 
-    public Client(int id, String nom, String prenom, String email, String address) {
+    public Client(int id, String nom, String prenom, String email, String address, String password) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
         this.email = email;
         this.address = address;
+        this.password = password;
     }
 
     // Getters
@@ -59,4 +62,19 @@ public class Client {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Client [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email + ", address=" + address
+                + "]";
+    }
+
 }
